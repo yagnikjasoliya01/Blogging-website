197a44e58b784534fae7111fb93839fea9cde4eb
pratikjetani05 <pratikjetani14@gmail.com>
2024-12-24 22:42:30 +0530

    e

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
-import Editor from "./pages/editor.pages";
+// import Editor from "./pages/editor.pages";
 import { createContext, useEffect, useState } from "react";
+import { lookInSession } from "./common/session";
 
 export const UserContext = createContext({});
 
@@ -19,7 +20,7 @@ const App = () => {
   return (
     <UserContext.Provider value={{ userAuth, setUserAuth }}>
       <Routes>
-        <Route path="/editor" element={<Editor />} />
+        {/* <Route path="/editor" element={<Editor />} /> */}
         <Route path="/" element={<Navbar />}>
           <Route path="signin" element={<UserAuthForm type="sign-in" />} />
           <Route path="signup" element={<UserAuthForm type="sing-up" />} />
