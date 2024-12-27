191789b6945b403063fc05a114b42b6a552cb22b
Yagnik Jasoliya <jasoliyayagnik38@gmail.com>
2024-12-27 18:37:24 +0530

    l

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -15,29 +15,40 @@ const UserAuthForm = ({ type }) => {
     setUserAuth,
   } = useContext(UserContext);
 
-    const userAuthThroughServer = (serverRoute, fromData) => {
-        axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, fromData)
-        .then(({data}) => {
-            storeInSession("user", JSON.stringify(data));
+  // const userAuthThroughServer = (serverRoute, fromData) => {
+  //   axios
+  //     .post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, fromData)
+  //     .then(({ data }) => {
+  //       storeInSession("user", JSON.stringify(data));
 
-            setUserAuth(data)
-        })
-        .catch(({response}) => {
-            toast.error(response.data.error);
-        })
+  //       setUserAuth(data);
+  //     })
+  //     .catch(({ response }) => {
+  //       toast.error(response.data.error);
+  //     });
+  // };
+  const userAuthThroughServer = async (serverRoute, formData) => {
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}${serverRoute}`, formData);
+      storeInSession('user', JSON.stringify(data));
+      setUserAuth(data);
+    } catch (error) {
+      toast.error(error.response?.data?.error || 'An error occurred. Try again.');
     }
+  };
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-        let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+    let serverRoute = type == "sign-in" ? "/signin" : "/signup";
 
-        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
-        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
-        //formData
-        let form = new FormData(formElement);
-        let formData = {};
+    //formData
+    let form = new FormData(formElement);
+    let formData = {};
 
     for (let [key, value] of form.entries()) {
       formData[key] = value;
@@ -45,65 +56,86 @@ const UserAuthForm = ({ type }) => {
 
     let { fullname, email, password } = formData;
 
-        //form validation 
-        if(fullname){
-            if(fullname.length < 3){
-                return toast.error("Fullname must be atleast 3 letters long");
-            }
-        }
-        if(!email.length){
-            return toast.error("Enter Email");
-        }
-        if(!emailRegex.test(email)){
-            return toast.error("Email is invalid");
-        }
-        if(!passwordRegex.test(password)){
-            return toast.error("password should be 6 to 20 character long with numeric, 1 lowercase and 1 uppercase letters");
-        }
-
-        userAuthThroughServer(serverRoute , formData);
+    //form validation
+    if (fullname) {
+      if (fullname.length < 3) {
+        return toast.error("Fullname must be atleast 3 letters long");
+      }
+    }
+    if (!email.length) {
+      return toast.error("Enter Email");
+    }
+    if (!emailRegex.test(email)) {
+      return toast.error("Email is invalid");
+    }
+    if (!passwordRegex.test(password)) {
+      return toast.error(
+        "password should be 6 to 20 character long with numeric, 1 lowercase and 1 uppercase letters"
+      );
     }
 
-    return(
-        access_token ?
-        <Navigate to='/'/>
-        :
-        <AnimationWrapper keyValue={type}>
-            <section className="h-cover flex items-center justify-center ">
-            <Toaster />
-            <form id="formElement" className="w-[80%] max-w-[400px]">
-                <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-                    {type == "sign-in" ? "Welcome Back" : "Join us today"}
-                </h1>
-                {
-                    type != "sign-in" ? 
-                    <InputBox
-                     name="fullname"
-                    type="text"
-                    placeholder="Full Name"
-                    icon="fi-rr-user"
-                    /> :""
-                }
-                <InputBox
-                    name="email"
-                    type="email"
-                    placeholder="E-mail"
-                    icon="fi-rr-envelope"
-                    />
-                <InputBox
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    icon="fi-rr-key"
-                    />
+    userAuthThroughServer(serverRoute, formData);
+  };
 
-                    <button 
-                        className="btn-dark center mt-14" 
-                        type="submit"
-                        onClick={handleSubmit}
-                    >
-                        {type.replace("-" , " ")}
-                    </button>
+  const handleGoogleAuth = (e) => {
+    e.preventDefault();
+    
+    authWithGoogle().then(user => {
+      let serverRoute = "/google-auth"
+
+      let formData = {
+        access_token: user.access_token
+      }
+
+      userAuthThroughServer(serverRoute, formData);
+
+    })
+    .catch(err => {
+      toast.error('Trouble login through google');
+      return console.log(err);
+    })
+  }
+
+  return access_token ? (
+    <Navigate to="/" />
+  ) : (
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex items-center justify-center ">
+        <Toaster />
+        <form id="formElement" className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type == "sign-in" ? "Welcome Back" : "Join us today"}
+          </h1>
+          {type != "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Full Name"
+              icon="fi-rr-user"
+            />
+          ) : (
+            ""
+          )}
+          <InputBox
+            name="email"
+            type="email"
+            placeholder="E-mail"
+            icon="fi-rr-envelope"
+          />
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Password"
+            icon="fi-rr-key"
+          />
+
+          <button
+            className="btn-dark center mt-14"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            {type.replace("-", " ")}
+          </button>
 
           <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
             <hr className="w-1/2 border-black" />
@@ -111,32 +143,32 @@ const UserAuthForm = ({ type }) => {
             <hr className="w-1/2 border-black" />
           </div>
 
-                    <button className="btn-dark flex items-center justify-center gap-4 w-[90%]  center">   
-                        <img src={googleicon} alt="icon of google" className="w-5" />
-                        contine with google
-                    </button>
+          <button className="btn-dark flex items-center justify-center gap-4 w-[90%]  center"
+            onClick={handleGoogleAuth}
+          >
+            <img src={googleicon} alt="icon of google" className="w-5" />
+            contine with google
+          </button>
 
-                    {
-                        type == "sign-in" ?
-                        <p className="mt-6 text-dark-grey text-xl text-center">
-                            Don't have an account?
-                            <Link to="/signup" className="underline text-black text-xl ml-1">
-                                Join us today
-                            </Link>
-                        </p>
-                        :
-                        <p className="mt-6 text-dark-grey text-xl text-center">
-                            Already a member
-                            <Link to="/signin" className="underline text-black text-xl ml-1">
-                                Sign in here.
-                            </Link>
-                        </p>
-                    }
-            </form>
-
-        </section>
-        </AnimationWrapper>
-    )
-}
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Don't have an account?
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+                Join us today
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Already a member
+              <Link to="/signin" className="underline text-black text-xl ml-1">
+                Sign in here.
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </AnimationWrapper>
+  );
+};
 
 export default UserAuthForm;
